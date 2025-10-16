7928c7e8ffa2106a8d0c03685fdc701aeaae0639
codewith-isha <codeoncanvas20@gmail.com>
2025-10-16 14:42:29 +0530

    navbar-styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import './Navbar.css'
 import MenuIcon from '@mui/icons-material/Menu';
+import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
+import SearchIcon from '@mui/icons-material/Search';
 import YouTubeIcon from '@mui/icons-material/YouTube';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+import PersonIcon from '@mui/icons-material/Person';
+
 
 
 const Navbar = () => {
@@ -14,9 +20,32 @@ const Navbar = () => {
       <div className='navbar_youtubeImg'>
         <YouTubeIcon sx={{fontSize:"34px"}} className='navbar_youtubeImage'/>
         <div className="navbar_utubeTitle">YouTube</div>
-
       </div>
     </div>
+
+    <div className='navbar-middle'>
+      <div className='navbar_searchBox'>
+        <input type="text" placeholder='search' className='navbar_searchBoxInput' />
+        <div className="navbar_searchIconBox">
+          <SearchIcon sx={{fontSize:"28px"}}/>
+        </div>
+
+        <div className="navbar_mike">
+          <KeyboardVoiceIcon sx={{color:"white"}}/>
+        </div>
+      </div>
+    </div>
+
+    <div className="navbar-right">
+      <VideoCallIcon sx={{fontSize:'30px', cursor:"pointer" , color:"white"}}/>
+       <NotificationsIcon sx={{fontSize:'30px', cursor:"pointer" , color:"white"}}/>
+        <PersonIcon sx={{fontSize:'30px', cursor:"pointer" , color:"white"}}/>
+
+    </div>
+
+
+
+
     </div>
   )
 }
